46e24e374a588c59f5b56a2210c3ff111961e838
saulram <saul.ramirez@disolutionsmx.com>
2020-05-07 00:46:30 -0500

    cambio de puerto al 2424

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port= process.env.port || 3977;
+var port= process.env.port || 2424;
 
 mongoose.connect('mongodb://localhost:27017/rewards',(err,res)=>{
     if(err){
